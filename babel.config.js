8e63ca437876c62fbad6cece6b9266d789462bc5
Mo Javad <jkhazali@gmail.com>
2024-01-19 16:53:56 +0000

    Adding RNX-Kit serializer

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,20 @@
+const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          disableImportExportTransform:
+            env === "production" && process.env["RNX_METRO_SERIALIZER_ESBUILD"],
+        },
+      ],
+    ],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
     ],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,22 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const {
+  MetroSerializer,
+  esbuildTransformerConfig,
+} = require("@rnx-kit/metro-serializer-esbuild");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 });
+
+config.serializer = {
+  customSerializer: MetroSerializer(undefined, {
+    target: "es6",
+  }),
+};
+
+config.transformer = esbuildTransformerConfig;
 
 module.exports = config;
